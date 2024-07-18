24db210d204c3c6f4ead7627c7b97a9188387f7d
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-18 15:50:17 +0530

    feat: Use rapidapi to fetch data for home page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,14 +1,27 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
 import SideBar from "./SideBar";
 import Videos from "./Videos";
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
+
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+    }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" }, backgroundColor: "#000"}}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 }}}>
-        <SideBar />
+        <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff"}}>
           Copyright 2024 Evil Corp
         </Typography>
@@ -19,7 +32,7 @@ const Feed = () => {
           New <span style={{ color: "#F32503"}}>videos</span>
         </Typography>
 
-        <Videos />
+        <Videos videos={videos} />
       </Box>
     </Stack>
   )
