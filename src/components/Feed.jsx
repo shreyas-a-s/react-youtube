8886fbb8e997111e1aec746d9165a14cb52cc89f
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-18 13:23:39 +0530

    feat: Add SideBar to Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
+import SideBar from "./SideBar";
+
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" }}}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 }}}>
-        SideBar
+        <SideBar />
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff"}}>
           Copyright 2024 Evil Corp
         </Typography>
